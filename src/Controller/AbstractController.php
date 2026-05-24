0cb6c05b8292d753ffa8b05b6e1354853a9016f3
Newball <newball875@gmail.com>
2026-05-24 09:31:15 +0200

    Correction oubli dans AbstractController

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -8,7 +8,6 @@ use Newball\Common\Service\ApiReponse;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 
 abstract class AbstractController extends SymfonyController {
 	/**
@@ -52,9 +51,5 @@ abstract class AbstractController extends SymfonyController {
 	 */
 	protected function successAudio(string $file, int $code = 200): Response{
 		return ApiReponse::audio($file, $code);
-	}
-
-	protected function getUserId(Request $req): ?int{
-
 	}
 }
